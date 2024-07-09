7a757a5edd2e57f50e883da6b79e3a360e7c4340
Elufere Innocent <eluferedonatus@gmail.com>
2024-07-09 22:07:23 +0100

    commit

--- api/routes/review.js
+++ api/routes/review.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { verifyToken } from "../middleware/jwt.js";
-import { createReview, getReview, deleteReview } from "../controller/review.js"
+import { createReview, getReviews, deleteReview } from "../controller/review.js";
 
 const router = express.Router();
 
-router.post("/", verifyToken, createReview);
-router.get("/:id", getReview);
-router.delete("/:id", verifyToken, deleteReview);
+router.post("/", verifyToken, createReview )
+router.get("/:gigId", getReviews )
+router.delete("/:id", deleteReview)
 
 export default router;
